d10b7371a5fce3cf907ae3252654a372fefe7ca3
kevingilfether <kevingilfether@gmail.com>
2017-10-05 15:13:48 -0400

    set up a property

--- OOP_Practice/Cat.cs
+++ OOP_Practice/Cat.cs
@@ -8,5 +8,27 @@ namespace OOP_Practice
 {
     class Cat
     {
+        //States
+        //fields
+        private string name;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //Behaviors
+
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor --
+            //takes not parameters
+
+        }
+
     }
 }
