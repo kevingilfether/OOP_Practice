d48a04e74cb1e5ddee31d27405121e1e3fd987cf
kevingilfether <kevingilfether@gmail.com>
2017-10-09 11:26:02 -0400

    Added some to Dog class

---
+++ OOP_Practice/Dog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Practice
+{
+    class Dog
+    {
+        //fields - In camelCase
+        private string hairLength;
+        private float height;
+        private string runningSpeed;
+        private int weight;
+
+        //Properties - In PascalCase
+        public string HairLength
+        {
+            get { return this.hairLength; }
+            set { this.hairLength = value; }
+        }
+
+        public Dog()
+        {
+
+        }
+
+
+        public void Run()
+        {
+
+        }
+
+        public void Bark()
+        {
+
+        }
+        public void Potty()
+        {
+
+        }
+        public void Cuddle()
+        {
+
+        }
+
+    }
+}
